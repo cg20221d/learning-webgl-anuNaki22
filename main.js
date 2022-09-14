56cde286c9b52a2b66e57f3581c2a2df16e84a88
anuNaki22 <rfarisakbar@gmail.com>
2022-09-14 15:11:15 +0700

    gambar 3 titik

--- main.js
+++ main.js
@@ -3,11 +3,23 @@ function main(){
     var kanvas = document.getElementById("kanvas");
     var gl = kanvas.getContext("webgl");
 
+    var vertices = [
+        0.5, 0.5,   // A: kanan atas
+        0.0, 0.0,   // B: bawah tengah 
+        -0.5, 0.5   // C: kiri atas
+    ];
+
+    var buffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+
+
     //vertex shader
     var vertexShaderCode = `
+    attribute vec2 aPosition;
     void main(){
-        float x = 0.0;
-        float y = 0.0;
+        float x = aPosition.x;
+        float y = aPosition.y;
         gl_PointSize = 10.0;
         gl_Position = vec4(x, y, 0.0, 1.0);
     }
@@ -36,9 +48,14 @@ function main(){
     gl.attachShader(shaderProgram, fragmentShaderObject);
     gl.linkProgram(shaderProgram);
     gl.useProgram(shaderProgram);
+    
+    // Menagajari GPU bagaimana caranya mengoleksi nilai posisi dari ARRAY_BUFFER untuk setiap verteks yang sedang diproses
+    var aPosition = gl.getAttribLocation(shaderProgram, "aPosition");
+    gl.vertexAttribPointer(aPosition, 2, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(aPosition);
 
     gl.clearColor(1.0, 0.65, 0.0, 1.0); // (R, G, B, Transparancy)
     gl.clear(gl.COLOR_BUFFER_BIT);
 
-    gl.drawArrays(gl.POINTS, 0, 1);
+    gl.drawArrays(gl.POINTS, 0, 3);
 }
