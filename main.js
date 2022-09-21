2b98cef3ea32cc81f22b93bf255a2d1894709606
anuNaki22 <rfarisakbar@gmail.com>
2022-09-21 14:03:16 +0700

    Persegi berwarna berputar

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
-/** @type {HTMLCanvasElement} */
 function main(){
+    /** @type {HTMLCanvasElement} */
     var kanvas = document.getElementById("kanvas");
     var gl = kanvas.getContext("webgl");
 
     var vertices = [
-        0.5, 0.5,   // A: kanan atas
-        0.0, 0.0,   // B: bawah tengah 
-        -0.5, 0.5,  // C: kiri atas
-        0.0, 1.0    // D: atas tengah
+        0.5, 0.5, 0.0, 1.0, 1.0,  // A: kanan atas    (BIRU LANGIT)
+        0.0, 0.0, 1.0, 0.0, 1.0,  // B: bawah tengah  (MAGENTA)
+        -0.5, 0.5, 1.0, 1.0, 1.0, // C: kiri atas     (KUNING)
+        0.0, 1.0, 1.0, 1.0, 1.0    // D: atas tengah   (PUTIH)
     ];
 
     var buffer = gl.createBuffer();
@@ -18,11 +18,14 @@ function main(){
     //vertex shader
     var vertexShaderCode = `
     attribute vec2 aPosition;
+    attribute vec3 aColor;
+    uniform float uTheta;
+    varying vec3 vColor;
     void main(){
-        float x = aPosition.x;
-        float y = aPosition.y;
-        gl_PointSize = 10.0;
+        float x = -sin(uTheta) * aPosition.x + cos(uTheta) * aPosition.y;
+        float y = sin(uTheta) * aPosition.y + cos(uTheta) * aPosition.x;
         gl_Position = vec4(x, y, 0.0, 1.0);
+        vColor = aColor;
     }
     `;
     var vertexShaderObject = gl.createShader(gl.VERTEX_SHADER);
@@ -31,13 +34,10 @@ function main(){
 
     //fragment shader
     var fragmentShaderCode = `
+    precision mediump float;
+    varying vec3 vColor;
     void main(){
-        precision mediump float;
-        float r = 0.0;
-        float g = 0.0;
-        float b = 1.0;
-        gl_FragColor = vec4(r, g, b, 1.0);
-
+        gl_FragColor = vec4(vColor, 1.0);
     }
     `;
     var fragmentShaderObject = gl.createShader(gl.FRAGMENT_SHADER);
@@ -50,13 +50,32 @@ function main(){
     gl.linkProgram(shaderProgram);
     gl.useProgram(shaderProgram);
     
+    //Variabel lokal
+    var theta = 0.0;
+
+    //Variabel pointer ke GLSL
+    var uTheta = gl.getUniformLocation(shaderProgram, "uTheta");
+
     // Menagajari GPU bagaimana caranya mengoleksi nilai posisi dari ARRAY_BUFFER untuk setiap verteks yang sedang diproses
     var aPosition = gl.getAttribLocation(shaderProgram, "aPosition");
-    gl.vertexAttribPointer(aPosition, 2, gl.FLOAT, false, 0, 0);
+    gl.vertexAttribPointer(aPosition, 2, gl.FLOAT, false, 
+        5 * Float32Array.BYTES_PER_ELEMENT, 
+        0);
     gl.enableVertexAttribArray(aPosition);
 
-    gl.clearColor(1.0, 0.65, 0.0, 1.0); // (R, G, B, Transparancy)
-    gl.clear(gl.COLOR_BUFFER_BIT);
+    var aColor = gl.getAttribLocation(shaderProgram, "aColor");
+    gl.vertexAttribPointer(aColor, 3, gl.FLOAT, false, 
+        5 * Float32Array.BYTES_PER_ELEMENT, 
+        2 * Float32Array.BYTES_PER_ELEMENT);
+    gl.enableVertexAttribArray(aColor);
 
-    gl.drawArrays(gl.TRIANGLE_FAN, 0, 4);
+    function render() {
+        gl.clearColor(1.0, 0.65, 0.0, 1.0); // (R, G, B, Transparancy)
+        gl.clear(gl.COLOR_BUFFER_BIT);
+        theta += 0.1;
+        gl.uniform1f(uTheta, theta);
+        
+        gl.drawArrays(gl.TRIANGLE_FAN, 0, 4);
+    }
+    setInterval(render, 1000/60);
 }
